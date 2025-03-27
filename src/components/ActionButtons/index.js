5520c6416bbfc3740bf6560087500cef6b4f6b23
chaerimjo-web <chaerim010710@gmail.com>
2025-03-27 13:47:11 +0900

    버튼

--- src/components/ActionButtons/index.js
+++ src/components/ActionButtons/index.js
@@ -1,9 +1,9 @@
-function ActionButtons(){
+function ActionButtons({step, questionsLength}){
+	const isLast = questionsLength - 1 === step;
 	return(
 		<div>
-			<button>이전</button>
-			<button>다음</button>
-			<button>제출</button>
+			 {step === 0 || <button>이전</button>}
+			 {isLast ? <button>제출</button> : <button>다음</button>  }
 		</div>
 	)
 }
