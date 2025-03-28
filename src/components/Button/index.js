5de3b31b02b328dedabd1e34a026beff1ab3123c
chaerimjo-web <chaerim010710@gmail.com>
2025-03-28 12:54:30 +0900

    버튼 컴포넌트 생성

---
+++ src/components/Button/index.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import { PRIMARY, SECONDARY, TERTIARY } from "../Constants";
+
+const colorMap = {
+  PRIMARY,
+  SECONDARY,
+  TERTIARY,
+};
+
+const Button = styled.button`
+  width: 200px;
+  border-radius: 4px;
+
+  background: ${({ type }) => colorMap[type].BUTTON.DEFAULT.BACKGROUND};
+  color: ${({ type }) => colorMap[type].BUTTON.DEFAULT.COLOR};
+  font-weight: bold;
+  font-size: 18px;
+  border: none;
+  padding: 16px 24px;
+
+  border: ${({ type = "TERTIARY" }) =>
+    (colorMap[type] || colorMap["TERTIARY"]).BUTTON.DEFAULT.BORDER};
+
+  &:hover {
+    background: ${({ type }) => colorMap[type].BUTTON.HOVER.BACKGROUND};
+    color: ${({ type }) => colorMap[type].BUTTON.HOVER.COLOR};
+    border: ${({ type }) =>
+      type === "TERTIARY"
+        ? `1px solid ${TERTIARY.BUTTON.HOVER.BORDER}`
+        : "none"};
+  }
+  &:active {
+    background: ${({ type }) => colorMap[type].BUTTON.PRESSED.BACKGROUND};
+    color: ${({ type }) => colorMap[type].BUTTON.PRESSED.COLOR};
+    border: ${({ type }) =>
+      type === "TERTIARY"
+        ? `1px solid ${TERTIARY.BUTTON.PRESSED.BORDER}`
+        : "none"};
+  }
+  &:disabled {
+    background: ${({ type }) => colorMap[type].BUTTON.DISABLED.BACKGROUND};
+    color: ${({ type }) => colorMap[type].BUTTON.DISABLED.COLOR};
+    border: ${({ type }) =>
+      type === "TERTIARY"
+        ? `1px solid ${TERTIARY.BUTTON.DISABLED.BORDER}`
+        : "none"};
+  }
+`;
+
+export default Button;
---
+++ src/components/Constants/index.js
@@ -0,0 +1,59 @@
+export const PRIMARY = {
+  BUTTON: {
+    DEFAULT: {
+      BACKGROUND: "#6542F1",
+      COLOR: "#FFFFFF",
+    },
+    HOVER: {
+      BACKGROUND: "#9982F4",
+    },
+    PRESSED: {
+      BACKGROUND: "#4B31B0",
+    },
+    DISABLED: {
+      BACKGROUND: "#EDEDED",
+      COLOR: "#D0CDCD",
+    },
+  },
+};
+export const SECONDARY = {
+  BUTTON: {
+    DEFAULT: {
+      BACKGROUND: "#DEDEDE",
+      COLOR: "#3A3A3A",
+    },
+    HOVER: {
+      BACKGROUND: "#F3F3F3",
+    },
+    PRESSED: {
+      BACKGROUND: "#B8B7B9",
+    },
+    DISABLED: {
+      BACKGROUND: "#EDEDED",
+      COLOR: "#D0CDCD",
+    },
+  },
+};
+export const TERTIARY = {
+  BUTTON: {
+    DEFAULT: {
+      BACKGROUND: "#FFFFFF",
+      COLOR: "#6542F1",
+      BORDER: "1px solid #6542F1",
+    },
+    HOVER: {
+      BACKGROUND: "#FFFFFF",
+      COLOR: "#9982F4",
+      BORDER: "1px solid #9982F4",
+    },
+    PRESSED: {
+      BACKGROUND: "#F0EDFC",
+      COLOR: "#4B31B0",
+      BORDER: "1px solid #4B31B0",
+    },
+    DISABLED: {
+      BACKGROUND: "#F6F6F6",
+      COLOR: "#D0CDCD",
+    },
+  },
+};
