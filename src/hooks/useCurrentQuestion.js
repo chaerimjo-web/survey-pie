920e767a8e24d1fd66cdf26ac248c4eb649029ec
chaerimjo-web <chaerim010710@gmail.com>
2025-04-02 15:54:53 +0900

    useRecoilState

--- src/hooks/useCurrentQuestion.js
+++ src/hooks/useCurrentQuestion.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 import {useEffect} from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 
 import surveyState from "../store/survey/atom";
 import useStep from './useStep'
 
 function useCurrentQuestion(){
 	const step = useStep();
-  const surveyData = useRecoilValue(surveyState);
+  const [surveyData, setSurvey] = useRecoilState(surveyState);
 	const questions = surveyData?.questions || []; 
 	
 	useEffect(()=>{
 		axios.get('http://localhost:3001/surveys/1').then((res)=>{
 			console.log(res);
+			setSurvey(res.data)
 		});
-	}, []);
+	}, [setSurvey]);
 
 	return questions[step];
 }
