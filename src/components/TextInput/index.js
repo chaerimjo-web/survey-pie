386f08f2c2c4c2bf6a52dab110eb42429ae51272
chaerimjo-web <chaerim010710@gmail.com>
2025-04-03 16:06:42 +0900

    글자 수 제한

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-function TextInput({ answer='', setAnswer, options }) {
+function TextInput({ answer = "", setAnswer, options }) {
   return (
     <div>
       <Input
@@ -10,6 +10,11 @@ function TextInput({ answer='', setAnswer, options }) {
           setAnswer(e.target.value);
         }}
         placeholder={options.placeholder}
+        {...(options?.max && { maxLength: options?.max })}
+        //맥스가 언디파인이아닌 경우에는 뒤에것을 실행
+        //언디파인이면 아예 없는 문장을 취급한다.
+        // {...(undefined && {maxLength: 1})} -> 언디파인드 되어서 maxLength 없음
+        // maxLength={options?.max}
       />
     </div>
   );
@@ -17,7 +22,7 @@ function TextInput({ answer='', setAnswer, options }) {
 
 const Input = styled.input`
   border-radius: 5px;
-  border: 1px solid #E0E0E0;
+  border: 1px solid #e0e0e0;
   padding: 12px 18px;
   font-size: 18px;
   font-weight: 400;
@@ -25,7 +30,8 @@ const Input = styled.input`
   box-sizing: boder-box;
   resize: none;
   width: 100%;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 `;
 
 export default TextInput;
