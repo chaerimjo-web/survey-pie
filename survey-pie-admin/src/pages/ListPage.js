d1d946f6a7c70d020d52f9870ccd517df683f4f2
chaerimjo-web <chaerim010710@gmail.com>
2025-04-07 19:47:46 +0900

    페이지네이션 추가

--- survey-pie-admin/src/pages/ListPage.js
+++ survey-pie-admin/src/pages/ListPage.js
@@ -1,42 +1,55 @@
-import {Table} from "antd";
+import { Table } from "antd";
+// import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useSWR from "swr";
 
 import MainLayout from "../layouts/MainLayout";
 import fetcher from "../libs/fetcher";
 
-const columns = [
-	{
-		title: '번호',
-		dataIndex: 'id',
-		key: 'id',
-	},
-	{
-		title: '제목',
-		dataIndex: 'title',
-		key: 'title',
-	},
-	{
-		title: '생성일',
-		dataIndex: 'createdAt',
-		key: 'createdAt',
-		render: (createdAt)=>{
-			const time = new Date(createdAt);
-			return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-		}
-	},
-	{
-		title: '액션',
-		dataIndex: 'id',
-		key: 'action',
-		render: (id)=>{
-			return <button>삭제</button>
-		}
-	},
-];
+const PAGE_SIZE = 2;
 
+const columns = [
+  {
+    title: "번호",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "제목",
+    dataIndex: "title",
+    key: "title",
+  },
+  {
+    title: "생성일",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: (createdAt) => {
+      const time = new Date(createdAt);
+      return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    },
+  },
+  {
+    title: "액션",
+    dataIndex: "id",
+    key: "action",
+    render: (id) => {
+      return (
+        <button
+          onClick={() => {
+            console.log(id, "삭제");
+          }}
+        >
+          삭제
+        </button>
+      );
+    },
+  },
+];
 
 function ListPage() {
   const { data, error } = useSWR("/surveys", fetcher);
+  const navigate = useNavigate();
+  // const [page, setPage] = useState();
 
   console.log(data);
 
@@ -44,14 +57,33 @@ function ListPage() {
     return "error";
   }
 
-	if(!data){
-		return 'loading ...';
-	}
+  if (!data) {
+    return "loading ...";
+  }
 
   return (
-		<MainLayout selectedKeys={["list"]}>
-			<Table columns={columns} dataSource={data.map((item)=>({...item, key: item.id}))} />;
-		</MainLayout>
-	);
+    <MainLayout selectedKeys={["list"]}>
+      <Table
+        pagination={{
+          pageSize: PAGE_SIZE,
+          // total: data.length,
+          // current: page,
+        }}
+        // onChange={(pagination)=>{
+        // 	setPage(pagination.current);
+        // }}
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => {
+              console.log(record, rowIndex);
+              navigate(`/builder/${record.id}`);
+            },
+          };
+        }}
+        columns={columns}
+        dataSource={data.map((item) => ({ ...item, key: item.id }))}
+      />
+    </MainLayout>
+  );
 }
 export default ListPage;
