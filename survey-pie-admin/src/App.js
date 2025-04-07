a62f822e0bdb6640cd7f65bd3d1e4b438b1f0d19
chaerimjo-web <chaerim010710@gmail.com>
2025-04-07 14:46:58 +0900

    라우터

--- survey-pie-admin/src/App.js
+++ survey-pie-admin/src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+import BuilderPage from "./pages/BuilderPage";
 import ListPage from "./pages/ListPage";
 
 function App() {
-  return <div className="App">
-    <ListPage/>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ListPage/>} />
+        <Route path="/list" element={<ListPage/>} />
+        <Route path="/builder" element={<BuilderPage/>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- survey-pie-admin/src/index.js
+++ survey-pie-admin/src/index.js
@@ -3,13 +3,16 @@ import "./index.css";
 import { DatePicker } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <DatePicker />
+    <BrowserRouter>
+      <App />
+      <DatePicker />
+    </BrowserRouter>
   </React.StrictMode>
 );
