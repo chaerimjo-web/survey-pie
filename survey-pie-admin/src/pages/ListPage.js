aa05263093d40a764b30af56b64c6893336d2a8f
chaerimjo-web <chaerim010710@gmail.com>
2025-04-07 17:09:10 +0900

    데이터 추가

--- survey-pie-admin/src/pages/ListPage.js
+++ survey-pie-admin/src/pages/ListPage.js
@@ -1,7 +1,39 @@
+import {Table} from "antd";
 import useSWR from "swr";
 
 import MainLayout from "../layouts/MainLayout";
 import fetcher from "../libs/fetcher";
+
+const columns = [
+	{
+		title: '번호',
+		dataIndex: 'id',
+		key: 'id',
+	},
+	{
+		title: '제목',
+		dataIndex: 'title',
+		key: 'title',
+	},
+	{
+		title: '생성일',
+		dataIndex: 'createdAt',
+		key: 'createdAt',
+		render: (createdAt)=>{
+			const time = new Date(createdAt);
+			return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+		}
+	},
+	{
+		title: '액션',
+		dataIndex: 'id',
+		key: 'action',
+		render: (id)=>{
+			return <button>삭제</button>
+		}
+	},
+];
+
 
 function ListPage() {
   const { data, error } = useSWR("/surveys", fetcher);
@@ -12,6 +44,14 @@ function ListPage() {
     return "error";
   }
 
-  return <MainLayout selectedKeys={["list"]}>ListPage</MainLayout>;
+	if(!data){
+		return 'loading ...';
+	}
+
+  return (
+		<MainLayout selectedKeys={["list"]}>
+			<Table columns={columns} dataSource={data.map((item)=>({...item, key: item.id}))} />;
+		</MainLayout>
+	);
 }
 export default ListPage;
