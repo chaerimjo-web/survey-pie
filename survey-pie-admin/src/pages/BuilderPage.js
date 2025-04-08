402e1faee3deeabcce2bfacfbb3ba63a38e78e2a
chaerimjo-web <chaerim010710@gmail.com>
2025-04-08 18:01:05 +0900

    antd 그리드 설정

--- survey-pie-admin/src/pages/BuilderPage.js
+++ survey-pie-admin/src/pages/BuilderPage.js
@@ -1,6 +1,15 @@
+import { Col, Row } from "antd";
+
 import MainLayout from "../layouts/MainLayout";
 
-function BuilderPage(){
-	return <MainLayout selectedKeys={["builder"]}>BuilderPage</MainLayout>
+function BuilderPage() {
+  return (
+		<MainLayout selectedKeys={["builder"]}>
+			<Row>
+				<Col flex="auto">Preview</Col>
+				<Col flex="300px">Option</Col>
+			</Row>
+		</MainLayout>
+	);
 }
 export default BuilderPage;
