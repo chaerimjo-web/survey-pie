5543c0e394298b4c63809f2137492b7f73513ac5
chaerimjo-web <chaerim010710@gmail.com>
2025-04-10 19:31:01 +0900

    카드 삭제 및 이동하기

--- survey-pie-admin/src/components/Card/index.js
+++ survey-pie-admin/src/components/Card/index.js
@@ -1,6 +1,8 @@
+import { DeleteOutlined, DownOutlined, UpOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import { styled } from "styled-components";
 
-function Card({ title, desc, children }) {
+function Card({ title, desc, children, onUpButtonClick, onDownButtonClick, deleteButtonClick }) {
   return (
     <CardWrapper>
       <Head>
@@ -8,15 +10,47 @@ function Card({ title, desc, children }) {
         <Desc>{desc}</Desc>
       </Head>
       <Body>{children}</Body>
+      <ButtonGroupWrapper>
+        <ButtonGroup>
+          <Button onClick={onUpButtonClick} type="text" icon={<UpOutlined />} />
+          <Button
+            onClick={deleteButtonClick}
+            type="text"
+            icon={<DeleteOutlined />}
+          />
+          <Button
+            onClick={onDownButtonClick}
+            type="text"
+            icon={<DownOutlined />}
+          />
+        </ButtonGroup>
+      </ButtonGroupWrapper>
     </CardWrapper>
   );
 }
 
+const ButtonGroupWrapper = styled.div`
+  position: absolute;
+  left: calc(100%);
+  top: 0;
+  display: none;
+`;
+
+const ButtonGroup = styled.div`
+  margin-left: 10px;
+  background: #fff;
+`;
+
 const CardWrapper = styled.div`
   border: 1px solid #dddddd;
-	background: #ffffff;
-	width: 500px;
-	margin: 30px auto;
+  background: #ffffff;
+  width: 500px;
+  margin: 30px auto;
+  position: relative;
+
+  &:hover ${ButtonGroupWrapper} {
+    display: block;
+  }
 `;
 const Head = styled.div`
   border-bottom: 1px solid #dddddd;
@@ -28,7 +62,7 @@ const Title = styled.div`
 `;
 const Desc = styled.div`
   color: #666666;
-	margin-left: 5px;
+  margin-left: 5px;
 `;
 const Body = styled.div`
   padding: 20px;
--- survey-pie-admin/src/components/PreviewSection/index.js
+++ survey-pie-admin/src/components/PreviewSection/index.js
@@ -2,15 +2,28 @@ import AddButton from "../AddButton";
 import Body from "../Body";
 import Card from "../Card";
 
-function PreviewSection({questions, addQuestion}) {
-  return ( 
+function PreviewSection({
+  questions,
+  addQuestion,
+  moveUpQuestion,
+  moveDownQuestion,
+  DeleteQuestion,
+}) {
+  return (
     <div>
-			{questions.map((question, index)=>(
-				<Card key={index} title={question.title} desc={question.desc}>
-					<Body type={question.type} options={question.options} />
-				</Card>
-			))}
-			<AddButton onClick={addQuestion} />
+      {questions.map((question, index) => (
+        <Card
+          key={index}
+          title={question.title}
+          desc={question.desc}
+          onUpButtonClick={()=>moveUpQuestion(index)}
+          onDownButtonClick={()=>moveDownQuestion(index)}
+          deleteButtonClick={()=>DeleteQuestion(index)}
+        >
+          <Body type={question.type} options={question.options} />
+        </Card>
+      ))}
+      <AddButton onClick={addQuestion} />
     </div>
   );
 }
