4a20e0fb61cc76cbabe389217284b0dc74a0c7a8
chaerimjo-web <chaerim010710@gmail.com>
2025-04-15 19:16:00 +0900

    Title 스타일 추가

--- survey-pie-admin/src/components/OptionSection/index.js
+++ survey-pie-admin/src/components/OptionSection/index.js
@@ -1,7 +1,12 @@
 import { styled } from "styled-components";
 
 function OptionSection() {
-  return <OptionSectionWrapper>OptionSection</OptionSectionWrapper>;
+  return (
+		<OptionSectionWrapper>
+			<Title>
+				문항 옵션
+			</Title>
+		</OptionSectionWrapper>);
 }
 
 const OptionSectionWrapper = styled.div`
@@ -9,5 +14,12 @@ const OptionSectionWrapper = styled.div`
 	border-left: 1px solid #dddddd;
 	background: #fff;
 `;
+const Title = styled.div`
+	padding: 10px 0;
+	background: #f0f0f0;
+	text-align: center;
+	font-weight: 500;
+	border-bottom: 1px solid #dddddd;
+`
 
 export default OptionSection;
