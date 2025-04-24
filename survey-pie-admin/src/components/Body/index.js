92d09c97df2d6c76638de81fd8ee814781f29939
chaerimjo-web <chaerim010710@gmail.com>
2025-04-24 16:21:38 +0900

    null 추가수정

--- survey-pie-admin/src/components/Body/index.js
+++ survey-pie-admin/src/components/Body/index.js
@@ -2,15 +2,17 @@ import SelectInput from "../SelectInput";
 import TextAreaInput from "../TextAreaInput";
 import Text from "../TextInput";
 
-function Body({type, options}){
-	let Components;
-	if(type === 'text'){
-		Components = Text;
-	}else if(type === 'textarea'){
-		Components = TextAreaInput;
-	}else if(type === 'select'){
-		Components = SelectInput;
-	}
-	return <Components options={options}/>;
+function Body({ type, options }) {
+  let Components;
+  if (type === "text") {
+    Components = Text;
+  } else if (type === "textarea") {
+    Components = TextAreaInput;
+  } else if (type === "select") {
+    Components = SelectInput;
+  } else {
+    return null;
+  }
+  return <Components options={options} />;
 }
 export default Body;
