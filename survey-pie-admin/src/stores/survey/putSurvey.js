0bfa79908c5333a9dd8650374dd0f8023ad2855d
chaerimjo-web <chaerim010710@gmail.com>
2025-04-30 16:00:54 +0900

    url 이름 수정

--- survey-pie-admin/src/stores/survey/putSurvey.js
+++ survey-pie-admin/src/stores/survey/putSurvey.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function putSurvey(survey) {
-  axios.put(`/survey/${survey.id}`, survey).then("저장이 되었습니다.");
+  axios.put(`/surveys/${survey.id}`, survey).then("저장이 되었습니다.");
 }
 
 export default putSurvey;
