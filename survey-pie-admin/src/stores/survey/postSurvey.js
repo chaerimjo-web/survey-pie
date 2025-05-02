387ef789559131825096022e9b2eb0fc2282d1c5
chaerimjo-web <chaerim010710@gmail.com>
2025-05-02 17:00:46 +0900

    함수, 기간

--- survey-pie-admin/src/stores/survey/postSurvey.js
+++ survey-pie-admin/src/stores/survey/postSurvey.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 function postSurvey(survey) {
-  return axios.post("/surveys", survey).then((res) => {
-    alert("저장이 되었습니다.");
-    return res.data;
-  });
+console.log(survey)
+  return axios
+    .post("/surveys", { ...survey, createdAt: new Date().getTime() })
+    .then((res) => {
+      alert("저장이 되었습니다.");
+
+      return res.data;
+    });
 }
 
 export default postSurvey;
