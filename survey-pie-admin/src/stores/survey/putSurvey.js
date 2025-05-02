51033e6792ef28a07e1946e2cf3857aa8196b0ff
chaerimjo-web <chaerim010710@gmail.com>
2025-05-02 11:58:27 +0900

    함수 수정

--- survey-pie-admin/src/stores/survey/putSurvey.js
+++ survey-pie-admin/src/stores/survey/putSurvey.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 function putSurvey(survey) {
-  axios.put(`/surveys/${survey.id}`, survey).then("저장이 되었습니다.");
+  axios
+    .put(`/surveys/${survey.id}`, survey)
+    .then(() => alert("저장이 되었습니다."));
 }
 
 export default putSurvey;
