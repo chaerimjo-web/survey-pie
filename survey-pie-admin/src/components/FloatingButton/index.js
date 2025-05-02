c32c87e210f775be0c94bfaabbf18871bc6fb5f8
chaerimjo-web <chaerim010710@gmail.com>
2025-05-02 11:59:05 +0900

    페이지 이동 함수 추가

--- survey-pie-admin/src/components/FloatingButton/index.js
+++ survey-pie-admin/src/components/FloatingButton/index.js
@@ -1,15 +1,27 @@
 import { Button } from "antd";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
+import postSurvey from "../../stores/survey/postSurvey";
 import putSurvey from "../../stores/survey/putSurvey";
 
 function FloatingButton() {
   const survey = useSelector((state) => state.survey.data);
-	
+  const navigate = useNavigate();
+  const isEditPage = !!survey.id;
+
   return (
     <FloatingButtonWrapper>
-      <Button onClick={() => putSurvey(survey)}>저장</Button>
+      <Button
+        onClick={() =>
+          isEditPage
+            ? putSurvey(survey)
+            : postSurvey(survey).then((data) => navigate(`/builder/${data.id}`))
+        }
+      >
+        저장
+      </Button>
       {/* survey 데이터 useSelect를 이용하여 가져오기 */}
     </FloatingButtonWrapper>
   );
